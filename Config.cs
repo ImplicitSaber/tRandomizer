76d8adc2687db90bed7e81986c36b614f89a11f3
ImplicitSaber <jacob.rutledge0509@gmail.com>
2025-06-19 23:07:27 -0500

    Version 2.0.0

--- Config.cs
+++ Config.cs
@@ -8,95 +8,64 @@ namespace tRandomizer
         public override ConfigScope Mode => ConfigScope.ServerSide;
 
         [Header("Randomization")]
-        [Label("Randomize monster and boss drops")]
-        [Tooltip("Setting this to true will randomize all NPC drops. This includes monsters and bosses! If this is set to false, no randomization of drops will take place.")]
         [DefaultValue(true)]
         [ReloadRequired]
-        public bool randomizeMobDrops;
+        public bool RandomizeMobDrops;
 
-        [Label("Randomize NPC shops")]
-        [Tooltip("Setting this to true will randomize all NPC shops. This will randomize the items and price. If this is set to false, no randomization of shop items will take place.")]
         [DefaultValue(true)]
         [ReloadRequired]
-        public bool randomizeNPCShops;
+        public bool RandomizeNPCShops;
 
-        [Label("Randomize boss HP")]
-        [Tooltip("Setting this to true will randomize all bosses' health. If this is set to false, no randomization of boss HP will take place.")]
         [DefaultValue(true)]
         [ReloadRequired]
-        public bool randomizeBossHealth;
+        public bool RandomizeBossHealth;
 
-        [Label("Randomize boss damage")]
-        [Tooltip("Setting this to true will randomize all bosses' damage. If this is set to false, no randomization of boss damage will take place.")]
         [DefaultValue(true)]
         [ReloadRequired]
-        public bool randomizeBossDamage;
+        public bool RandomizeBossDamage;
 
-        [Label("Don't randomize progression required items")]
-        [Tooltip("Setting this to true will stop the randomization of items required for progression, e.g. guide voodoo doll, pwnhammer. This will not matter if Randomize monster and boss drops is false.")]
         [DefaultValue(true)]
         [ReloadRequired]
-        public bool dontRandomizeProgressionReqdItems;
+        public bool DontRandomizeReqdItems;
 
-        [Label("Minimum random boss health")]
-        [Tooltip("The minimum amount of boss health the randomizer will select.")]
         [DefaultValue(1000)]
         [ReloadRequired]
-        public int minRandomBossHealth;
+        public int MinRandomBossHealth;
 
-        [Label("Maximum random boss health")]
-        [Tooltip("The maximum amount of boss health the randomizer will select.")]
         [DefaultValue(1000000)]
         [ReloadRequired]
-        public int maxRandomBossHealth;
+        public int MaxRandomBossHealth;
 
-        [Label("Minimum random boss damage")]
-        [Tooltip("The minimum amount of boss damage the randomizer will select.")]
         [DefaultValue(1)]
         [ReloadRequired]
-        public int minRandomBossDamage;
+        public int MinRandomBossDamage;
 
-        [Label("Maximum random boss damage")]
-        [Tooltip("The maximum amount of boss damage the randomizer will select.")]
         [DefaultValue(500)]
         [ReloadRequired]
-        public int maxRandomBossDamage;
+        public int MaxRandomBossDamage;
 
-        [Label("Maximum random defender medal amount")]
-        [Tooltip("The maximum amount of defender medals a shop will sell an item for.")]
-        [DefaultValue(999)]
-        [ReloadRequired]
-        public int maxRandomDefMedals;
-
-        [Label("Maximum random platinum amount")]
-        [Tooltip("The maximum amount of platinum a shop will sell an item for.")]
         [DefaultValue(50)]
         [ReloadRequired]
-        public int maxRandomPlatinum;
+        public int MaxRandomPlatinum;
 
-        [Label("Maximum random gold amount")]
-        [Tooltip("The maximum amount of gold a shop will sell an item for.")]
         [DefaultValue(99)]
         [ReloadRequired]
-        public int maxRandomGold;
-
-        [Label("Maximum random silver amount")]
-        [Tooltip("The maximum amount of silver a shop will sell an item for.")]
+        public int MaxRandomGold;
         [DefaultValue(99)]
         [ReloadRequired]
-        public int maxRandomSilver;
+        public int MaxRandomSilver;
 
-        [Label("Maximum random copper amount")]
-        [Tooltip("The maximum amount of copper a shop will sell an item for.")]
         [DefaultValue(99)]
         [ReloadRequired]
-        public int maxRandomCopper;
+        public int MaxRandomCopper;
 
-        [Label("Randomize the results of recipes")]
-        [Tooltip("Randomizes the outputs of recipes. Note: Randomization of recipe outputs will not take place for progression required items if mentioned setting is enabled!")]
         [DefaultValue(true)]
         [ReloadRequired]
-        public bool randomizeRecipeResults;
+        public bool RandomizeRecipeResults;
+
+        [DefaultValue(false)]
+        [ReloadRequired]
+        public bool RespectMaxStack;
 
     }
 }
--- RandomizerSystem.cs
+++ RandomizerSystem.cs
@@ -2,30 +2,29 @@
 using Terraria;
 using System;
 using Terraria.ID;
-using System.Collections.Generic;
-using Terraria.WorldBuilding;
-using Terraria.GameContent.Generation;
-using Terraria.IO;
 
 namespace tRandomizer
 {
 	public class RandomizerSystem : ModSystem
 	{
-		public override void PostAddRecipes()
+
+        private static readonly Random rand = new Random(Guid.NewGuid().GetHashCode());
+
+        public override void PostAddRecipes()
 		{
-			bool rrR = ModContent.GetInstance<Config>().randomizeRecipeResults;
-			bool dnrPRI = ModContent.GetInstance<Config>().dontRandomizeProgressionReqdItems;
-			if (rrR)
+			Config cfg = ModContent.GetInstance<Config>();
+			if (cfg.RandomizeRecipeResults)
 			{
 				for (int i = 0; i < Recipe.numRecipes; i++)
 				{
 					Recipe r = Main.recipe[i];
 					Random rand = new Random(Guid.NewGuid().GetHashCode());
-					if((r.HasResult(ItemID.MechanicalEye) || r.HasResult(ItemID.MechanicalWorm) || r.HasResult(ItemID.MechanicalSkull)) && dnrPRI)
+					if((r.HasResult(ItemID.MechanicalEye) || r.HasResult(ItemID.MechanicalWorm) || r.HasResult(ItemID.MechanicalSkull)) && cfg.DontRandomizeReqdItems)
                     {
 						continue;
                     }
-					r.ReplaceResult(rand.Next(1, Main.item.Length), rand.Next(1, 1000));
+					int id = rand.Next(1, Main.item.Length);
+                    r.ReplaceResult(id, rand.Next(1, (cfg.RespectMaxStack ? ItemMaxStackGetter.GetItemMaxStack(id) : Item.CommonMaxStack) + 1));
 				}
 			}
 		}
